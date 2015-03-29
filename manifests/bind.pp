9c842265b27292942fd30a698f4d6863ba2381c4
ronen <narkisr@gmail.com>
2015-03-29 13:32:56 +0300

    bind vs unbind docs

--- manifests/bind.pp
+++ manifests/bind.pp
@@ -1,10 +1,19 @@
-# redis bind settings
+# Manage redis bind settings, you can either pass a bind value:
+# redis::bind {'allow all':
+#   bind => '0.0.0.0'
+# }
+# or remove the bind entry completely:
+# 
+# redis::bind {'allow all':
+#   unbind => true
+# }
 define redis::bind(
   $unbind=false,
   $bind = false
 ){
   
   if($bind) {
+    validate_string($bind)
     editfile::config { "bind ${bind}":
       ensure  => $bind,
       path    => $::redis::conf,
