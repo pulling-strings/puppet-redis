06e3d594e1a2e0fb4f55c38780253f99f5710096
ronen <narkisr@gmail.com>
2017-01-19 23:44:25 +1100

    moving to support 16.x

--- manifests/ubuntu.pp
+++ manifests/ubuntu.pp
@@ -2,7 +2,7 @@
 class redis::ubuntu{
   include apt
 
-  if $lsbdistrelease == '15.10' or $lsbdistrelease == '15.04' {
+  if $lsbdistrelease == '16.10' or $lsbdistrelease == '16.04' {
     package{'software-properties-common':
       ensure  => present
     } -> Exec <||>
